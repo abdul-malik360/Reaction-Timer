a37030a31a1bc9eb1fe789bfc4352f43b1992a74
Abdul-Malik <82204978+abdul-malik360@users.noreply.github.com>
2021-10-10 23:27:24 +0200

    first commit

---
+++ src/components/Block.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="block" v-if="showBlock" @click="stopTimer">
+    Click me
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["delay"],
+  data() {
+    return {
+      showBlock: false,
+      timer: null,
+      reactionTime: 0,
+    };
+  },
+  mounted() {
+    console.log("component mounted");
+    setTimeout(() => {
+      this.showBlock = true;
+      this.startTimer();
+      console.log(this.delay);
+    }, this.delay);
+  },
+  //   updated() {
+  //     console.log("component updated");
+  //   },
+  //   unmounted() {
+  //     console.log("unmounted");
+  //   },
+  methods: {
+    startTimer() {
+      this.timer = setInterval(() => {
+        this.reactionTime += 10;
+      }, 10);
+    },
+    stopTimer() {
+      clearInterval(this.timer);
+      console.log(this.reactionTime);
+      this.$emit("end", this.reactionTime);
+    },
+  },
+};
+</script>
+
+<style>
+.block {
+  width: 400px;
+  border-radius: 20px;
+  background: #0faf87;
+  color: white;
+  text-align: center;
+  padding: 100px 0;
+  margin: 40px auto;
+}
+
+button {
+  background: #0faf87;
+  color: white;
+  border: none;
+  padding: 8px 16px;
+  border-radius: 4px;
+  font-size: 16px;
+  letter-spacing: 1px;
+  cursor: pointer;
+  margin: 10px;
+}
+button[disabled] {
+  opacity: 0.2;
+  cursor: not-allowed;
+}
+</style>
